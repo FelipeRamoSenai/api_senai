f721bbc1a427a7a88fe04a6794f9244ff011ea05
Felipe Ramos de Carvalho Bartolazzi <PET0126917@aluno.firjansenaisesi.com.br>
2023-10-02 09:19:32 -0300

    api inicio.

---
+++ src/main/java/com/api_senai/api/ApiApplication.java
@@ -0,0 +1,17 @@
+package com.api_senai.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+
+		System.out.println("Hello Wolrd!");
+	}
+
+}
